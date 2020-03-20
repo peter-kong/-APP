204245291db20509ceabd9e7a67ad926dcff45b4
PeterKong0310 <410621208@gms.ndhu.edu.tw>
2020-03-20 22:28:54 +0800

    撥亂反正

--- Thematic/app/src/main/java/com/example/thematic/Test.java
+++ Thematic/app/src/main/java/com/example/thematic/Test.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity_register extends AppCompatActivity {
+public class Test extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        getSupportActionBar().hide(); //隱藏標題
+        setContentView(R.layout.activity_test);
     }
 }
