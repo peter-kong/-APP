0419f18e20cfa257323b33594c1d3676e2c5211f
Harrow6081 <ww0970884699@gmail.com>
2020-03-21 00:31:48 +0800

    no message

--- Thematic/app/src/main/java/com/example/thematic/MainActivity.java
+++ Thematic/app/src/main/java/com/example/thematic/MainActivity.java
@@ -41,5 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
--- Thematic/app/src/main/java/com/example/thematic/menu_for_manager.java
+++ Thematic/app/src/main/java/com/example/thematic/menu_for_manager.java
@@ -2,13 +2,31 @@ package com.example.thematic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menu_for_manager extends AppCompatActivity {
 
+    private Button button22;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_for_manager);
+
+
+        button22 = (Button) findViewById(R.id.button22);
+
+        Button registerBtn = (Button) findViewById(R.id.button22);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(menu_for_manager.this,CaregiverSelect.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
