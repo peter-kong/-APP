3464bc6f99e904eb920f3d604582421854138f65
TLin_88 <forest310246@gmail.com>
2020-03-20 15:12:01 +0800

    register_manager and user

--- Thematic/app/src/main/java/com/example/thematic/MainActivity.java
+++ Thematic/app/src/main/java/com/example/thematic/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, activity_register1.class);
+                intent.setClass(MainActivity.this, activity_register_choose.class);
                 startActivity(intent);
             }
         });
