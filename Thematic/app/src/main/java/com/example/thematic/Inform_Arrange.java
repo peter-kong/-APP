487bb3352aeaaf5cb3996d51fee6823d1dd6bb6d
PeterKong0310 <410621208@gms.ndhu.edu.tw>
2020-03-20 20:33:15 +0800

    排程通知

---
+++ Thematic/app/src/main/java/com/example/thematic/Inform_Arrange.java
@@ -0,0 +1,14 @@
+package com.example.thematic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Inform_Arrange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inform__arrange);
+    }
+}
