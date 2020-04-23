7d7b1a967f3ef64c26c110199a6b734337a87e74
TAITAI88 <z3607551@gmail.com>
2020-04-23 22:31:05 +0800

    0423

--- Thematic/app/src/main/java/com/example/thematic/Schedule_search1.java
+++ Thematic/app/src/main/java/com/example/thematic/Schedule_search1.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
 public class Schedule_search1 extends AppCompatActivity {
 
-    private Button schedule_morning;
+    private NumberPicker mNumberPicker_start_hour;
+    private NumberPicker mNumberPicker_start_min;
+    private NumberPicker mNumberPicker_end_hour;
+    private NumberPicker mNumberPicker_end_min;
+    private Button Schedule_Search_送出;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,31 @@ public class Schedule_search1 extends AppCompatActivity {
         getSupportActionBar().hide(); //隱藏標題
         setContentView(R.layout.activity_manager_schedule_search1);
 
-        schedule_morning = (Button) findViewById(R.id.schedule_morning);
-        Button nextBtn = (Button) findViewById(R.id.schedule_morning);
-        nextBtn.setOnClickListener(new View.OnClickListener() {
+        final String[] Min = getResources().getStringArray(R.array.Min);
+
+        mNumberPicker_start_hour = (NumberPicker) findViewById(R.id.numberPicker_start_hour);
+        mNumberPicker_start_hour.setMinValue(8);
+        mNumberPicker_start_hour.setMaxValue(17);
+        mNumberPicker_start_hour.setValue(10);
+
+        mNumberPicker_start_min = (NumberPicker) findViewById(R.id.numberPicker_start_min);
+        mNumberPicker_start_min.setMinValue(0);
+        mNumberPicker_start_min.setMaxValue(Min.length - 1);
+        mNumberPicker_start_min.setDisplayedValues(Min);
+
+
+        mNumberPicker_end_hour = (NumberPicker) findViewById(R.id.numberPicker_end_hour);
+        mNumberPicker_end_hour.setMinValue(8);
+        mNumberPicker_end_hour.setMaxValue(17);
+        mNumberPicker_end_hour.setValue(14);
+
+        mNumberPicker_end_min = (NumberPicker) findViewById(R.id.numberPicker_end_min);
+        mNumberPicker_end_min.setMinValue(0);
+        mNumberPicker_end_min.setMaxValue(Min.length - 1);
+        mNumberPicker_end_min.setDisplayedValues(Min);
+
+        Button NextpageBtn = (Button) findViewById(R.id.Schedule_Search_送出);
+        NextpageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
