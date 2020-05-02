58891febf5ed61068efb1e2df43535d64a6eff7d
TLin_88 <forest310246@gmail.com>
2020-05-02 20:29:09 +0800

    login

--- Thematic/app/src/main/java/com/example/thematic/Personal_data.java
+++ Thematic/app/src/main/java/com/example/thematic/Personal_data.java
@@ -33,34 +33,33 @@ public class Personal_data extends AppCompatActivity {
             @Override
             public void run(){
                 com.example.mysql_connect.MySQLCon con = new com.example.mysql_connect.MySQLCon();
-                Log.e("OK","Hello Line 34");
-
-                con.run();
+                //Log.e("OK","Hello Line 34");
+                //con.run();
                 //設定一個物件來裝從getData中拿到的東西
-                Log.e("OK","Hello");
+                //Log.e("OK","Hello");
                 final String account_get = con.getData(Account,"account_get");
                 //跟server說OK(?
-                Log.v("OK",account_get);
+                //Log.v("OK",account_get);
                 final String name_get = con.getData(Account,"name_get");
-                Log.v("OK",name_get);
+                //Log.v("OK",name_get);
                 /*
                 final String gender_get = con.getData(Account,"gender_get");
                 Log.v("OK",gender_get);
                 */
                 final String birthday_get = con.getData(Account,"birthday_get");
-                Log.v("OK",birthday_get);
+                //Log.v("OK",birthday_get);
                 final String level_get = con.getData(Account,"level_get");
-                Log.v("OK",level_get);
+                //Log.v("OK",level_get);
                 final String connectnum_get = con.getData(Account,"connectnum_get");
-                Log.v("OK",connectnum_get);
+                //Log.v("OK",connectnum_get);
                 final String email_get = con.getData(Account,"email_get");
-                Log.v("OK",email_get);
+                //Log.v("OK",email_get);
                 final String address_get = con.getData(Account,"address_get");
-                Log.v("OK",address_get);
+                //Log.v("OK",address_get);
                 final String healthsitu_get = con.getData(Account,"healthsitu_get");
-                Log.v("OK",healthsitu_get);
+                //Log.v("OK",healthsitu_get);
                 final String personid_get = con.getData(Account,"personid_get");
-                Log.v("OK",personid_get);
+                //Log.v("OK",personid_get);
 
                 //讓上面的變數來拿到從getData獲得的物件
                 account.post(new Runnable() {
