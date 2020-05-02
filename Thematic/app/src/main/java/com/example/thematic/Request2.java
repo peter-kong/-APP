b7728369248b65a68a2803487bc5e3a208aa676f
TAITAI88 <z3607551@gmail.com>
2020-05-02 21:55:17 +0800

    Add spinner

--- Thematic/app/src/main/java/com/example/thematic/Request2.java
+++ Thematic/app/src/main/java/com/example/thematic/Request2.java
@@ -3,13 +3,28 @@ package com.example.thematic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Request2 extends AppCompatActivity {
+
+    private Spinner Time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manager_request2);
         getSupportActionBar().hide(); //隱藏標題
+        setContentView(R.layout.activity_manager_request2);
+
+        Time = (Spinner)findViewById(R.id.Time_spinner);
+    }
+
+    public void  order(View v){
+        String[] Time = getResources().getStringArray(R.array.Time);
     }
 }
+
+
+
+
