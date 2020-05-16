39e253c5d81257e821c1c48787aaf86e8c315958
TAITAI88 <z3607551@gmail.com>
2020-05-17 00:39:12 +0800

    2020/5/17

--- Thematic/app/src/main/java/com/example/thematic/History_Inform_arrange.java
+++ Thematic/app/src/main/java/com/example/thematic/History_Inform_arrange.java
@@ -2,13 +2,157 @@ package com.example.thematic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.mysql.jdbc.StringUtils;
+
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.ArrayList;
 
 public class History_Inform_arrange extends AppCompatActivity {
+    String[] str = new String[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history__inform_arrange);
+        getSupportActionBar().hide(); //隱藏標題
+
+        setContentView(R.layout.activity_history_inform_arrange);
+        final TextView time_view = (TextView) findViewById(R.id.時間顯示);
+        final TextView caregiver_view = (TextView) findViewById(R.id.照服員名稱);
+        final TextView work_view = (TextView) findViewById(R.id.個人資料);
+        final TextView 日期 = (TextView) findViewById(R.id.歷史照服時間);
+        final Spinner 日期下拉選單 = (Spinner) findViewById(R.id.日期下拉選單);
+        //取全域的歷史日期的資料
+        GlobalVariable_Account obj1 = (GlobalVariable_Account) getApplicationContext();
+        ArrayList Historydate = obj1.returnDate();
+        String[] 歷史日期 = new String[Historydate.size()];
+        Historydate.toArray(歷史日期);
+        //將所有日期資料放入spinner中
+        ArrayAdapter datelist = new ArrayAdapter(History_Inform_arrange.this, android.R.layout.simple_spinner_item, 歷史日期);
+        datelist.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        日期下拉選單.setAdapter(datelist);
+        //spinner被選擇值的時候動作
+        日期下拉選單.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                //拿到被選擇项的值進行連接DB並顯示資料
+                String chooseDate = (String) 日期下拉選單.getSelectedItem();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        com.example.mysql_connect.MySQLCon con = new com.example.mysql_connect.MySQLCon();
+                        con.run();
+                        GlobalVariable_Account obj = (GlobalVariable_Account)getApplicationContext();
+                        String user帳號  = obj.returnAcc();
+                        String UID = con.get_ID(user帳號,"我要userID");
+                        ArrayList data = con.getschedule(chooseDate,"我要歷史工作內容",UID);
+                        if(data.size() == 0) {
+                            for (int i = 0; i < 5; i++) {
+                                data.add("無資料");
+                            }
+                        }
+                        time_view.post(new Runnable() {
+                            public void run() {
+                                time_view.setText(""+data.get(0)+"~"+data.get(1));
+                            }
+                        });
+                        caregiver_view.post(new Runnable() {
+                            public void run() {
+                                caregiver_view.setText(""+data.get(2));
+                            }
+                        });
+
+                        work_view.post(new Runnable() {
+                            public void run() {
+                                String work;
+                                work = ""+data.get(3);
+                                work = work.replace("、","\n");
+                                work_view.setText(work);
+                            }
+                        });
+                        日期.post(new Runnable() {
+                            public void run() {
+                                日期.setText("照服時間");
+                            }
+                        });
+                    }
+                }).start();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.e("nothingSelected","沒有選擇內容");
+            }
+        });
+
+
+        /*
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                日期下拉選單.setOnItemClickListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        String  chooseDate = 日期下拉選單.getSelectedItem().toString();
+                        GlobalVariable_Account 選擇歷史日期保存 = (GlobalVariable_Account)getApplicationContext();
+                        選擇歷史日期保存.setchooseDate(chooseDate);
+                        Intent intent = new Intent();
+                        intent.setClass(History_Inform_arrange.this, History_Inform_arrange.class);
+                        startActivity(intent);
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {}
+                });
+
+
+                String NeedDate;
+                ArrayList data = con.getschedule(NeedDate, "我要上次工作內容", user帳號);
+
+                time_view.post(new Runnable() {
+                    public void run() {
+                        time_view.setText("" + data.get(0) + "~" + data.get(1));
+                    }
+                });
+                caregiver_view.post(new Runnable() {
+                    public void run() {
+                        caregiver_view.setText("" + data.get(2));
+                    }
+                });
+
+                work_view.post(new Runnable() {
+                    public void run() {
+                        String work;
+                        work = "" + data.get(3);
+                        work = work.replace("、", "\n");
+                        work_view.setText(work);
+                    }
+                });
+                日期.post(new Runnable() {
+                    public void run() {
+                        日期.setText("照服時間:" + data.get(4));
+                    }
+
+
+                });
+
+
+            }
+        }).start();
+
+         */
     }
 }
--- Thematic/app/src/main/java/com/example/thematic/Personal_data.java
+++ Thematic/app/src/main/java/com/example/thematic/Personal_data.java
@@ -3,6 +3,8 @@ package com.example.thematic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class Personal_data extends AppCompatActivity {
 
@@ -10,5 +12,128 @@ public class Personal_data extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
+        getSupportActionBar().hide(); //隱藏標題
+        //設定變數 並在layout中找到相對應的東西
+        //Log.e("OK","Hello Line 40");
+        final TextView account    = (TextView) findViewById(R.id.個人資料_帳號);
+        final TextView name       = (TextView) findViewById(R.id.個人資料_姓名);
+        final TextView gender     = (TextView) findViewById(R.id.個人資料_性別);
+        final TextView birthday   = (TextView) findViewById(R.id.個人資料_生日);
+        final TextView level      = (TextView) findViewById(R.id.個人資料_失能等級);
+        final TextView connectnum = (TextView) findViewById(R.id.個人資料_連絡電話);
+        final TextView email      = (TextView) findViewById(R.id.個人資料);
+        final TextView address    = (TextView) findViewById(R.id.個人資料_住址);
+        final TextView healthsitu = (TextView) findViewById(R.id.個人資料_健康狀況);
+        final TextView personid   = (TextView) findViewById(R.id.個人資料_身分證字號);
+
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+
+                GlobalVariable_Account obj = (GlobalVariable_Account)getApplicationContext();
+
+                String Account  = obj.returnAcc();
+                com.example.mysql_connect.MySQLCon con = new com.example.mysql_connect.MySQLCon();
+                con.run();
+                Log.e("38","Hi");
+                //Log.e("OK","Hello Line 34");
+                //con.run();
+                //設定一個物件來裝從getData中拿到的東西
+                //Log.e("OK","Hello");
+                final String account_get = Account;
+                Log.e("44","Hi");
+                //跟server說OK(?
+                //Log.v("OK",account_get);
+                final String name_get = con.getData(Account,"name_get");
+
+                //Log.v("OK",name_get);
+
+                final String gender_get = con.getData(Account,"gender_get");
+                Log.v("OK",gender_get);
+
+                final String birthday_get = con.getData(Account,"birthday_get");
+
+
+                //Log.v("OK",birthday_get);
+                final String level_get = con.getData(Account,"level_get");
+
+                //Log.v("OK",level_get);
+                final String connectnum_get = con.getData(Account,"connectnum_get");
+
+                //Log.v("OK",connectnum_get);
+                final String email_get = con.getData(Account,"email_get");
+
+                //Log.v("OK",email_get);
+                final String address_get = con.getData(Account,"address_get");
+
+                //Log.v("OK",address_get);
+                final String healthsitu_get = con.getData(Account,"healthsitu_get");
+
+                //Log.v("OK",healthsitu_get);
+                final String personid_get = con.getData(Account,"personid_get");
+
+                //Log.v("OK",personid_get);
+
+                //讓上面的變數來拿到從getData獲得的物件
+                account.post(new Runnable() {
+                    public void run() {
+                        if(account_get.equals("")){
+                            account.setText("無資料");
+                        }
+                        else{
+                            account.setText(account_get);
+                        }
+                    }
+                });
+                name.post(new Runnable() {
+                    public void run() {
+                        name.setText(name_get);
+                    }
+                });
+                //gender db not yet
+
+                gender.post(new Runnable() {
+                    public void run() {
+                        gender.setText(gender_get);
+                    }
+                });
+                birthday.post(new Runnable() {
+                    public void run() {
+                        birthday.setText(birthday_get);
+                    }
+                });
+                level.post(new Runnable() {
+                    public void run() {
+                        level.setText(""+level_get);
+                    }
+                });
+                connectnum.post(new Runnable() {
+                    public void run() {
+                        connectnum.setText(connectnum_get);
+                    }
+                });
+                email.post(new Runnable() {
+                    public void run() {
+                        email.setText(email_get);
+                    }
+                });
+                address.post(new Runnable() {
+                    public void run() {
+                        address.setText(address_get);
+                    }
+                });
+                healthsitu.post(new Runnable() {
+                    public void run() {
+                        healthsitu.setText(healthsitu_get);
+                    }
+                });
+                personid.post(new Runnable() {
+                    public void run() {
+                        personid.setText(personid_get);
+                    }
+                });
+            }
+        }).start();
+
     }
 }
